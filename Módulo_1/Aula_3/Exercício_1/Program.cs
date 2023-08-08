9e1c5beabb6e91968be4e527420faa5ec7ef5138
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2023-08-08 07:17:25 -0300

    programa gerenciamento estoque produto

--- Módulo_1/Aula_3/Exercício_1/Program.cs
+++ Módulo_1/Aula_3/Exercício_1/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Produto produto = new Produto();
+
+            try
+            {
+                Console.WriteLine("Insira o nome do produto:");
+                produto.Nome = Console.ReadLine();
+                Console.WriteLine("Insira o preço do produto:");
+                produto.Preco = double.Parse(Console.ReadLine());
+                Console.WriteLine("Insira a quantidade do produto:");
+                produto.Quantidade = int.Parse(Console.ReadLine());
+
+                produto.RelatorioProduto();
+
+                Console.WriteLine("Insira a quantidade de produto que deseja adicionar ao estoque:");
+                int _quantidade = int.Parse(Console.ReadLine());
+                produto.AdicionarProdutos(_quantidade);
+
+
+                produto.RelatorioProduto();
+
+                Console.WriteLine("Insira a quantidade de produto que deseja remover do estoque:");
+                _quantidade = int.Parse(Console.ReadLine());
+                produto.RemoverProdutos(_quantidade);
+
+
+                produto.RelatorioProduto();
+
+            } catch (Exception)
+            {
+                Console.WriteLine("O valor inserido está incorreto");
+            }
+
         }
     }
 }
