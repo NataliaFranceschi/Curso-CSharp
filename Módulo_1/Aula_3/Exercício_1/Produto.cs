facaffcd89f3a22683b9adcf331edad80148c3a8
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2023-08-08 06:23:22 -0300

    feat: criação classe Produto

---
+++ Módulo_1/Aula_3/Exercício_1/Produto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_1
+{
+    public class Produto
+    {
+        string Nome = "";
+        double Preco = 0;
+        int Quantidade = 0;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int _quantidade)
+        {
+            Quantidade += _quantidade;
+        }
+
+        public void RemoverProdutos(int _quantidade)
+        {
+            if (_quantidade > Quantidade)
+            {
+                Console.WriteLine("Quantidade em estoque não é suficiente");
+            } else
+            {
+                Quantidade -= _quantidade;
+            }
+        }
+
+    }
+}
