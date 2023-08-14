c143b43030da11277cea671ffff1499d41abbb89
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2023-08-14 06:58:58 -0300

    feat: crianção classe Multa

---
+++ Módulo_1/Aula_5/Biblioteca/Multa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    internal class Multa
+    {
+        public double valorMultaDia { get; set; } = 2;
+        public bool valorPendente { get; set; } = false;
+        public double valorMulta { get; set; } = 0;
+       
+        public Multa() { }
+        
+        public void CalcularMulta(int diasAtraso)
+        {
+            valorMulta = diasAtraso * valorMultaDia;
+            valorPendente = true;
+        }
+
+        public void PagarMulta()
+        {
+            valorPendente = false;
+        }
+    }
+}
