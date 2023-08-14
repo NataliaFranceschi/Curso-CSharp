4f1ac409df95a0dd8003931acce0c6a2ea2ad059
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2023-08-14 07:00:54 -0300

    feat: programa gerenciamento biblioteca

--- Módulo_1/Aula_5/Biblioteca/Program.cs
+++ Módulo_1/Aula_5/Biblioteca/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Pessoa pessoa = new("Fernando", "Silva", new DateOnly(1995, 5, 20));
+            Pessoa pessoa2 = new("Gisele", "Santos", new DateOnly(1989, 4, 07), "gisele@gmail.com");
+         
+            Livro livro = new("Rapido e Devagar", "Daniel Kahneman", "Objetiva");
+            Livro livro2 = new("Essencialismo", "Greg McKeown");
+
+            Emprestimo emprestimo = new(livro, pessoa);
+            pessoa.AdicionarEmprestimo(emprestimo);
+            Emprestimo emprestimo2 = new(livro, pessoa2);
+            emprestimo.Devolver();
+            Emprestimo emprestimo3 = new(livro2, pessoa);
+
+            Console.ReadKey();
         }
     }
 }
