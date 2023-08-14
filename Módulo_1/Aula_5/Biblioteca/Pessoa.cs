45f3a71c561f3c969002f4705d4c3994dc8c61d6
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2023-08-14 06:57:53 -0300

    feat: criação classe Pessoa

---
+++ Módulo_1/Aula_5/Biblioteca/Pessoa.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    internal class Pessoa
+    {
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public DateOnly DataDeNascimento { get; private set; }
+        public string? Email { get; set; }
+        public List<Emprestimo> Emprestimos { get; set; } = new();
+
+        public Pessoa(string nome, string sobrenome, DateOnly dataDeNascimento)
+        {
+            Nome = nome;
+            Sobrenome = sobrenome;
+            DataDeNascimento = dataDeNascimento;
+        }
+
+        public Pessoa(string nome, string sobrenome, DateOnly dataDeNascimento, string email) :
+            this(nome, sobrenome, dataDeNascimento)
+        {
+            Email = email;
+        }
+
+        public void AdicionarEmprestimo(Emprestimo emprestimo)
+        {
+            Emprestimos.Add(emprestimo);
+        }
+
+        public bool MultaPendente()
+        {
+            return Emprestimos.Any(e => e.Multa.valorPendente == true);
+           
+        }
+    }
+}
