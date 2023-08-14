8395d06607415f2cd0ff68c956b3ce436c4491ec
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2023-08-14 06:56:44 -0300

    feat: criação classe Livro

---
+++ Módulo_1/Aula_5/Biblioteca/Livro.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    internal class Livro
+    {
+        public string Titulo { get; private set; }
+        public string Autor { get; private set; }
+        public string? Editora { get; private set; }
+        public bool Disponivel { get; set; } = true;
+
+        public Livro(string titulo, string autor)
+        {
+            Titulo = titulo;
+            Autor = autor;
+        }
+
+        public Livro(string titulo, string autor, string editora) :
+            this(titulo, autor)
+        {
+            Editora = editora;
+        }
+
+    }
+}
