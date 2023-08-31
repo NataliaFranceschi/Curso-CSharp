a0d6f27a6ae2a32a363f46302b86398ff28374d8
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2023-08-31 18:27:22 -0300

    feat: criação enum Tipo

---
+++ Módulo_2/Exercícios_Revisão/Exercício_2/Tipo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_2
+{
+    internal enum Tipo 
+    {
+        MicroempreendedorIndividual,
+        EmpresaIndividual,
+        SociedadeLimitadaUnipessoal,
+        SociedadeLimitadaEmpresarial,
+        SociedadeSimples,
+        SociedadeAnonima
+    }
+}
