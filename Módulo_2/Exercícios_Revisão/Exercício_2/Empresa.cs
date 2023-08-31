be73bcd91c4f716b887996eeef657781feb5eafd
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2023-08-31 18:26:39 -0300

    feat: criação classe empresa

---
+++ Módulo_2/Exercícios_Revisão/Exercício_2/Empresa.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_2
+{
+    internal class Empresa
+    {
+        protected string NomeFantasia { get; set; }
+        protected string RazaoSocial { get; set; }
+        protected string CNPJ { get; set; }
+        protected Tipo Tipo { get; set; }
+
+        public Empresa(string nomeFantasia, string razaoSocial, string cNPJ, Tipo tipo)
+        {
+            NomeFantasia = nomeFantasia;
+            RazaoSocial = razaoSocial;
+            CNPJ = cNPJ;
+            Tipo = tipo;
+        }
+    }
+}
