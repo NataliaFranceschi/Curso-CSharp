7f5f4da5ab06f9da6af59f46c8ba835c2653be2a
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2023-08-31 18:24:26 -0300

    feat: programa de criação de empresas

---
+++ Módulo_2/Exercícios_Revisão/Exercício_1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercício_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Varejo varejo = new Varejo("MercadoI", "Supermercado", "98765432100001", Tipo.EmpresaIndividual);
+            Empreiteira empreiteira = new Empreiteira("EmpreiteiraI", "Empreiteira Ltda", "12345678900001", Tipo.SociedadeLimitadaEmpresarial);
+
+            varejo.InformacoesEmpresa();
+            varejo.RealizarVenda();
+            Console.WriteLine();
+            empreiteira.InformacoesEmpresa();
+            empreiteira.RealizarVenda();
+
+            Console.ReadKey();
+        }
+    }
+}
