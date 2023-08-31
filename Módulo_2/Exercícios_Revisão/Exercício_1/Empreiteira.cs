2e3b62494679b036bb3162cf725e2cbc1cd6c09a
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2023-08-31 18:22:52 -0300

    feat: criação classe empreiteira

---
+++ Módulo_2/Exercícios_Revisão/Exercício_1/Empreiteira.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_1
+{
+    internal class Empreiteira : Empresa
+    {
+        public Empreiteira(string nomeFantasia, string razaoSocial, string cNPJ, Tipo tipo)
+                     : base(nomeFantasia, razaoSocial, cNPJ, tipo) { }
+
+        public override void RealizarVenda()
+        {
+            Console.WriteLine("Venda de serviço realizada!");
+        }
+    }
+}
