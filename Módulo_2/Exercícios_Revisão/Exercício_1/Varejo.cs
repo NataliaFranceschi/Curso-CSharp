5e1c465c9c4162cabbc5cacc6bd15fc1934eb071
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2023-08-31 18:25:24 -0300

    feat: criação classe varejo

---
+++ Módulo_2/Exercícios_Revisão/Exercício_1/Varejo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_1
+{
+    internal class Varejo : Empresa
+    {
+        public Varejo(string nomeFantasia, string razaoSocial, string cNPJ, Tipo tipo) 
+                     : base(nomeFantasia, razaoSocial, cNPJ, tipo) { }
+
+        public override void RealizarVenda()
+        {
+            Console.WriteLine("Venda do produto realizada!");
+        }
+    }
+}
