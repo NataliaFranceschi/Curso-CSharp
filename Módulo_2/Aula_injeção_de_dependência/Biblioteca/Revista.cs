d25f1f0ef62ee116029eccc337a77bd9a03842ef
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2023-09-05 20:29:59 -0300

    feat: criação classe Revista

---
+++ Módulo_2/Aula_injeção_de_dependência/Biblioteca/Revista.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    internal class Revista : ItemBiblioteca
+    {
+        public int MesPublicacao { get; set; }
+
+        public Revista(string titulo, int anoDePublicacao, int numeroDePaginas, int mesPublicacao) 
+            : base(titulo, anoDePublicacao, numeroDePaginas)
+        {
+            MesPublicacao = mesPublicacao;
+        }
+    }
+}
