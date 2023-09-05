96b18fb85064a71c3c498b375fab6c725ee56345
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2023-09-05 20:30:13 -0300

    feat: criação classe Usuario

---
+++ Módulo_2/Aula_injeção_de_dependência/Biblioteca/Usuario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    internal class Usuario
+    {
+        public string Name { get; set; }
+        public string CPF { get; set; }
+        public string Celular { get; set; }
+        public string Email { get; set; }
+        public DateOnly DataNascimento { get; set; }
+
+        public Usuario(string name, string cPF, string celular, string email, DateOnly dataNascimento)
+        {
+            Name = name;
+            CPF = cPF;
+            Celular = celular;
+            Email = email;
+            DataNascimento = dataNascimento;
+        }
+    }
+}
