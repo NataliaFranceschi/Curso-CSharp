44a418dec3be04e5d4eb08e8667f6a1a60799004
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2023-09-05 20:29:17 -0300

    feat: criação classe Livro

---
+++ Módulo_2/Aula_injeção_de_dependência/Biblioteca/Livro.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    internal class Livro : ItemBiblioteca, IPodeSerEmprestado
+    {
+        public string Autor { get; set; }
+        public bool Disponivel { get; set; } = true;
+        public Livro(string titulo, int anoDePublicacao, int numeroDePaginas, string autor) 
+            : base(titulo, anoDePublicacao, numeroDePaginas)
+        {
+            Autor = autor;
+        }
+
+        public void Emprestar(Emprestimo emprestimo)
+        {
+            emprestimo.PrazoParaDevolucao = emprestimo.DataEmprestimo.AddDays(14);
+            Disponivel = false;
+            Console.WriteLine("Empréstimo realizado com sucesso!");
+        }
+
+        public void Devolver(Emprestimo emprestimo)
+        {
+            emprestimo.DataDevolucao = DateTime.Now;
+            Disponivel = true;
+
+            if (emprestimo.DataDevolucao.Day > emprestimo.PrazoParaDevolucao.Day)
+            {
+                int diasAtraso = (emprestimo.DataDevolucao.Day - emprestimo.PrazoParaDevolucao.Day);
+                Console.WriteLine($"Livro entregue com {diasAtraso} dias de atraso.");
+
+            }
+            else
+            {
+                Console.WriteLine("Devolução realizada com sucesso!");
+            }
+        }
+
+    }
+}
