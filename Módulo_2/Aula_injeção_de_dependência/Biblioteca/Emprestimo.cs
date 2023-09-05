cc0de5595df25d2cc1460cb44e9ec0bfca08b230
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2023-09-05 20:26:37 -0300

    feat: criação classe empréstimo

---
+++ Módulo_2/Aula_injeção_de_dependência/Biblioteca/Emprestimo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    internal class Emprestimo
+    {
+        public DateTime DataEmprestimo { get; set; } = DateTime.Now;
+        public DateTime PrazoParaDevolucao { get; set; }
+        public DateTime DataDevolucao { get; set; }
+        public ItemBiblioteca ItemBiblioteca { get; set; }
+        public Usuario Usuario { get; set; }
+
+        public Emprestimo(ItemBiblioteca itemBiblioteca, Usuario usuario)
+        {
+            ItemBiblioteca = itemBiblioteca;
+            Usuario = usuario;
+        }
+    }
+}
