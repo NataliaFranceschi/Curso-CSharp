0d661647035a9c03d3023b76b1de32af95e248f9
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2023-09-05 20:29:36 -0300

    feat: criação classe MidiaDigital

---
+++ Módulo_2/Aula_injeção_de_dependência/Biblioteca/MidiaDigital.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    internal class MidiaDigital : ItemBiblioteca, IPodeSerEmprestado
+    {
+        public bool Disponivel { get ; set; } = true;
+        public string Autor { get; set; }
+        public MidiaDigital(string titulo, int anoDePublicacao, int numeroDePaginas, string autor) 
+            : base(titulo, anoDePublicacao, numeroDePaginas)
+        {
+            Autor = autor;
+        }
+
+        public void Emprestar(Emprestimo emprestimo)
+        {
+            Console.WriteLine("Empréstimo realizado com sucesso!");
+        }
+
+        public void Devolver(Emprestimo emprestimo)
+        {
+            emprestimo.DataDevolucao = DateTime.Now;
+            Console.WriteLine("Devolução realizada com sucesso!");
+        }
+
+    }
+}
