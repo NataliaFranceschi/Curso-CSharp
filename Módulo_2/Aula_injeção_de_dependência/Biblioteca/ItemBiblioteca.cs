1da1e48134771569e35062b436d5def18a6682a0
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2023-09-05 20:28:50 -0300

    feat: criação classe ItemBiblioteca

---
+++ Módulo_2/Aula_injeção_de_dependência/Biblioteca/ItemBiblioteca.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    internal class ItemBiblioteca
+    {
+        public string id {  get; set; }
+        public string Titulo { get; set; }
+        public int AnoDePublicacao { get; set; }
+        public int NumeroDePaginas { get; set; }
+
+        public ItemBiblioteca(string titulo, int anoDePublicacao, int numeroDePaginas)
+        {
+            Titulo = titulo;
+            AnoDePublicacao = anoDePublicacao;
+            NumeroDePaginas = numeroDePaginas;
+            id = GerarId();
+        }
+
+        public string GerarId()
+        {
+            Guid id = Guid.NewGuid();
+            return id.ToString();
+        }
+    }
+}
