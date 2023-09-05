cee883036722aac95cb8211522050dc7895ed5de
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2023-09-05 20:27:23 -0300

    feat: criação interface PodeSerEmprestado

---
+++ Módulo_2/Aula_injeção_de_dependência/Biblioteca/IPodeSerEmprestado.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    internal interface IPodeSerEmprestado
+    {
+        bool Disponivel { get; set; }
+        void Emprestar(Emprestimo emprestimo);
+        void Devolver(Emprestimo emprestimo);
+    }
+}
